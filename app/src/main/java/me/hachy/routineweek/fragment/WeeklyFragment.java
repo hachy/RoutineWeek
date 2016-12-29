850f4dd242d98fc5229210195ada161d1dac3d4b
hachy <1613863+hachy@users.noreply.github.com>
2016-12-29 16:27:59 +0900

    use current day
    viewpagerに現在の曜日を適用する

--- app/src/main/java/me/hachy/routineweek/fragment/WeeklyFragment.java
+++ app/src/main/java/me/hachy/routineweek/fragment/WeeklyFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import me.hachy.routineweek.R;
 import me.hachy.routineweek.adapter.WeeklyPagerAdapter;
@@ -30,8 +34,12 @@ public class WeeklyFragment extends Fragment {
         TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
         ViewPager viewpager = (ViewPager) view.findViewById(R.id.view_pager);
 
+        Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+
         viewpager.setAdapter(new WeeklyPagerAdapter(getFragmentManager(), getActivity()));
         tabLayout.setupWithViewPager(viewpager);
+        viewpager.setCurrentItem(day - 2); // currentItemが0から始まるのとTablayoutが月曜(1)から始まるので-2
 
         return view;
     }
