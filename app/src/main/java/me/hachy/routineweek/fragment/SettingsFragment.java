9e0d3ff732227d52226019ad56358d6b53aaf93d
hachy <1613863+hachy@users.noreply.github.com>
2017-01-20 10:09:36 +0900

    use license-tools-plugin

--- app/src/main/java/me/hachy/routineweek/fragment/SettingsFragment.java
+++ app/src/main/java/me/hachy/routineweek/fragment/SettingsFragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import me.hachy.routineweek.R;
+import me.hachy.routineweek.activity.LicenseActivity;
 
 public class SettingsFragment extends Fragment {
 
@@ -22,7 +24,17 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        TextView license = (TextView) view.findViewById(R.id.open_source_license);
+        license.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(LicenseActivity.createIntent(getContext()));
+            }
+        });
+
+        return view;
     }
 
 }
