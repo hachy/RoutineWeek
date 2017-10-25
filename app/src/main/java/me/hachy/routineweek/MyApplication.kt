d13f5a5bebf3dfec460301fdb4f48bce45cb94f5
hachy <1613863+hachy@users.noreply.github.com>
2017-10-25 10:19:17 +0900

    initialize Mobileads/FirebaseAnalytics

--- app/src/main/java/me/hachy/routineweek/MyApplication.kt
+++ app/src/main/java/me/hachy/routineweek/MyApplication.kt
@@ -2,6 +2,8 @@ package me.hachy.routineweek
 
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
+import com.google.firebase.analytics.FirebaseAnalytics
 
 import io.realm.Realm
 import io.realm.RealmConfiguration
@@ -17,5 +19,9 @@ class MyApplication : Application() {
                 .build()
         //        Realm.deleteRealm(realmConfig); // Delete Realm between app restarts.
         Realm.setDefaultConfiguration(realmConfig)
+
+        FirebaseAnalytics.getInstance(this)
+
+        MobileAds.initialize(applicationContext, resources.getString(R.string.banner_ad_unit_id_test))
     }
 }
